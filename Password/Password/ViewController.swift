566b6880b3f0e394e0ee8adda9963dd3467be607
Alan Forneron <alan.forneron@naranja.com>
2023-07-26 21:00:03 -0300

    add password view

--- Password/Password/ViewController.swift
+++ Password/Password/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let newPasswordTextField = PasswordTextField(placeHolderText: "New password")
+    let stackView = UIStackView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        style()
+        layout()
     }
+}
 
-
+extension ViewController {
+    func style() {
+        newPasswordTextField.translatesAutoresizingMaskIntoConstraints = false
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+                stackView.axis = .vertical
+                stackView.spacing = 20
+    }
+    
+    func layout() {
+        view.addSubview(stackView)
+        stackView.addArrangedSubview(newPasswordTextField)
+        
+        NSLayoutConstraint.activate([
+                stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+                stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 2),
+                view.trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 2)
+            ])
+    }
 }
 
